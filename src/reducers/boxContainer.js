aa2438962718bbac8e4fd852d94aa3dd690882cb
kzajic <k.zajic@outlook.com>
2018-02-20 23:25:24 +0100

    reducer improvements

--- src/reducers/boxContainer.js
+++ src/reducers/boxContainer.js
@@ -70,24 +70,27 @@ export default function(state = {rows}, action) {
                 let play = function (row, time) {
                     setTimeout( () => {
                         let row_play = document.getElementsByClassName("board-row")[index + 1];
-                            row_play.classList.add("row-play");
+                            
                         row.map((box, index) => {
+                            
                             if(box[0].active) {
+                                row_play.classList.add("row-play");
                                 let audio = document.querySelector(`audio[data-key="${index}"]`);
                                 audio.currentTime=0;
                                 audio.play();
                                 let topBox = document.getElementsByClassName("top-box")[index];
                                 topBox.classList.add("box-play");
-                                    setTimeout( () => {
-                                        topBox.classList.remove("box-play");
-                                    }, 500);
+                                setTimeout( () => {
+                                    topBox.classList.remove("box-play");
+                                    let row_active =  document.getElementsByClassName("row-play");
+                                    if (row_active.length) {
+                                        row_active[0].classList.remove("row-play");
+                                        }
+                                }, 500);
                             }
                         })
+                        
                     }, time);
-                let row_active =  document.getElementsByClassName("row-play")[0];
-                if (row_active !== undefined) {
-                    row_active.classList.remove("row-play");
-                }
                 }
                 let time = 1000 * index;
                 play(row.row, time);
